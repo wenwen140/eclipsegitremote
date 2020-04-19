f9dd02728ef0a256768634c4f31c111a68c393c6
wenwen140 <272797371@qq.com>
2020-04-19 21:53:08 +0800

    合并了分支

--- gittest/src/com/snow/git/Test.java
+++ gittest/src/com/snow/git/Test.java
@@ -3,7 +3,7 @@ package com.snow.git;
 public class Test {
 	public static void main(String[] args) {
 		//123
-		//567
+		//567+890
 		System.err.println("git test");
 		System.err.println("git 222");
 	}
