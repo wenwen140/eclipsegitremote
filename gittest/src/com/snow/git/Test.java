6332b942b3284ff2b96bac2e6bffc25d044c2db3
wenwen140 <272797371@qq.com>
2020-04-19 22:06:23 +0800

    解决冲突

--- gittest/src/com/snow/git/Test.java
+++ gittest/src/com/snow/git/Test.java
@@ -3,11 +3,7 @@ package com.snow.git;
 public class Test {
 	public static void main(String[] args) {
 		//123
-<<<<<<< HEAD
 		//234789
-=======
-		//234
->>>>>>> branch 'master' of https://github.com/wenwen140/eclipsegitremote.git
 		System.err.println("git test");
 		System.err.println("git 222");
 	}
